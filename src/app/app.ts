b27394be754667f60bb1012d099d9b9a008f1197
DeveloperManishR <manishrawat0711@gmail.com>
2026-05-03 18:27:25 +0530

    small change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "node:path"
 import checkBoxRoutes from "./modules/checkbox/checkbox.route.js"
-import type { Application } from "express";
+import type { Application, Response } from "express";
 import authRoutes from "./modules/auth/auth.route.js"
 
 export function createServerApplication(): Application {
@@ -10,8 +10,8 @@ export function createServerApplication(): Application {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(express.static(path.resolve('./public')))
-
-    app.use('/api/auth',authRoutes)
+  
+  app.use('/api/auth',authRoutes)
   app.use('/api/checkbox',checkBoxRoutes)
 
   return app;
